dd8e979a9e66f336075d579caa2abfd198bcfc78
zabdielcead <zabdiel.cead@gmail.com>
2018-11-28 18:23:41 -0600

    suben heroes cards

--- dosspa/src/app/app.module.ts
+++ dosspa/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+
+
+// servicios
+import { HeroesService } from './services/heroes.service';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +28,14 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- dosspa/src/app/components/heroes/heroes.component.ts
+++ dosspa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    heroes: Heroe [] = [];
 
+  constructor( private _heroesService: HeroesService ) {
+    // constructor se ejecuta  mucho antes que el ngOnInit
+    console.log('constructor');
+  }
   ngOnInit() {
+    // es utilizado cuando la pagina ya esta rendrizada pero esta en el inicio como un constructor su comportamiento
+    console.log('ngOnInit');
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
